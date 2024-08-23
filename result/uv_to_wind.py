292b235ccf9bbdd4c7b5e68c2eb8b18a5275507a
Zongwei <58176200+Zhang-zongwei@users.noreply.github.com>
2024-08-23 20:47:05 +0800

    Update uv_to_wind.py

--- result/uv_to_wind.py
+++ result/uv_to_wind.py
@@ -3,12 +3,19 @@ import numpy as np
 
 #####用于反归一化以及求取风速
 
-uv = np.load(r'true.npy')
+uv_true = np.load(r'uv_true.npy')
+uv_pred = np.load(r'uv_pred.npy')
 
-u = uv[:, :, 0, :, :] * 3.520639587404695 + 1.3793918561383813
-v = uv[:, :, 1, :, :] * 4.020371225633169 - 0.1864951025062636
+u_true = uv_true[:, :, 0, :, :] * 3.520639587404695 + 1.3793918561383813
+v_true = uv_true[:, :, 1, :, :] * 4.020371225633169 - 0.1864951025062636
 
-w = np.sqrt(u ** 2 + v ** 2)
-np.save(r"true_w.npy", arr=w)
+u_pred = uv_pred[:, :, 0, :, :] * 3.520639587404695 + 1.3793918561383813
+v_pred = uv_pred[:, :, 1, :, :] * 4.020371225633169 - 0.1864951025062636
+
+w_true = np.sqrt(u_true ** 2 + v_true ** 2)
+w_pred = np.sqrt(u_pred ** 2 + v_pred ** 2)
+
+np.save(r"w_pred.npy", arr=w_pred)
+np.save(r"w_true.npy", arr=w_true)
 
 
